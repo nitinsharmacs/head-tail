092b49c4127836ad9de0d946048fb7cd279bc248
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-05-26 18:41:52 +0530

    Used process.exitCode instead of process.exit()

--- head.js
+++ head.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
-const { exit } = require('process');
 const { headMain } = require('./src/head/headLib.js');
 const { stderrMessage } = require('./src/head/errorHandler.js');
 
 const main = () => {
   try {
     const { log: logger, error: errorLogger } = console;
-    exit(
-      headMain(
-        fs.readFileSync,
-        process.argv.slice(2),
-        { logger, errorLogger }
-      )
+    process.exitCode = headMain(
+      fs.readFileSync,
+      process.argv.slice(2),
+      { logger, errorLogger }
     );
   } catch (error) {
     console.error(stderrMessage(error));
-    exit(1);
+    process.exitCode = 1;
   }
 };
 
--- tail.js
+++ tail.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
-const { exit } = require('process');
 const { tailMain } = require('./src/tail/tailMain.js');
 const { stderrMessage } = require('./src/tail/errorHandler.js');
 
 const main = () => {
   const { log: logger, error: errorLogger } = console;
   try {
-    exit(
-      tailMain(
-        fs.readFileSync,
-        process.argv.slice(2),
-        { logger, errorLogger }
-      )
+    process.exitCode = tailMain(
+      fs.readFileSync,
+      process.argv.slice(2),
+      { logger, errorLogger }
     );
   } catch (error) {
     errorLogger(stderrMessage(error));
-    exit(1);
+    process.exitCode = 1;
   }
 };
 
